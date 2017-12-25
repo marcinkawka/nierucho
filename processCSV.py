952202a905d09deb31d1e43b6c5aeb5d6e97f474
marcinkawka <kawka.marcin@gmail.com>
2017-12-25 19:44:54 +0100

    Ukończony odczyt z pliku CSV

--- processCSV.py
+++ processCSV.py
@@ -4,6 +4,7 @@
 #
 #TODO: Porządek z cenami
 from pprint import pprint
+from datetime import datetime
 import urllib, json
 import csv
 import sqlite3
@@ -16,38 +17,135 @@ db.text_factory = str
 cursor = db.cursor()
 
 with open('lokale2.csv','r') as csvfile:
-	spamreader = csv.reader(csvfile, delimiter=',')
+	reader = csv.reader(csvfile, delimiter=',')
 	i=0
-	for row in spamreader:
+	next(reader, None)  # skip the headers
+	for row in reader:
 		adres=(row[1].split("(")[0]).encode('utf8')
 		przezna=row[2].encode('utf8')
 		nazwa_podmiotu=row[3].encode('utf8')
-	'''	
+		i=i+1
+	
 		try:
-			pow1=float(row[4])
+			pow1=float(row[4].replace(',','.'))
 		except ValueError:
-			pow1 =-1
+			
+			pow1 =0 #powierzchnia w postaci stringa
+			pow1str = str(row[4])
 			
 		try:
-			pow2=float(row[5])
+			pow2=float(row[5].replace(',','.'))
 		except ValueError:
-			pow2 =-1
-			
+			pow2 =0
+			pow2str = str(row[5])
+		
+
 		usyt=row[6].encode('utf8')
 		info=row[7].encode('utf8')	
-		#masa błędów w komórkach z czynszem
+		
+		czynszStr=""
 		try:
-			czynsz17=float((row[16].split("(")[0]).replace(',','.')) #(brutto)
+			czynsz09=float(row[8].replace(',','.'))
 		except ValueError:
-			czynsz17=-1
+			czynsz09 =0 
+			if(row[8]): #in case there is a text info
+				czynszStr = czynszStr + str("2009: "+row[8]+"\n")
+		
+		try:
+			czynsz10=float(row[9].replace(',','.'))
+		except ValueError:
+			czynsz10 =0
+			if(row[9]): #in case there is a text info 
+				czynszStr = czynszStr + str("2010: "+row[9]+"\n")
+		
+		try:
+			czynsz11=float(row[10].replace(',','.'))
+		except ValueError:
+			czynsz11 =0
+			if(row[10]): #in case there is a text info 
+				czynszStr = czynszStr + str("2011: "+row[10]+"\n")
+		
+		try:
+			czynsz12=float(row[11].replace(',','.'))
+		except ValueError:
+			czynsz12 =0
+			if(row[11]): #in case there is a text info 
+				czynszStr = czynszStr + str("2012: "+row[11]+"\n")
+		
+		try:
+			czynsz13=float(row[12].replace(',','.'))
+		except ValueError:
+			czynsz13 =0
+			if(row[12]): #in case there is a text info 
+				czynszStr = czynszStr + str("2013: "+row[12]+"\n")
+		try:
+			czynsz14=float(row[13].replace(',','.'))
+		except ValueError:
+			czynsz14 =0
+			if(row[13]): #in case there is a text info 
+				czynszStr = czynszStr + str("2014: "+row[13]+"\n")
+		try:
+			czynsz15=float(row[14].replace(',','.'))
+		except ValueError:
+			czynsz15 =0
+			if(row[14]): #in case there is a text info 
+				czynszStr = czynszStr + str("2015: "+row[14]+"\n")
+		try:
+			czynsz16=float(row[15].replace(',','.'))
+		except ValueError:
+			czynsz16 =0
+			if(row[15]): #in case there is a text info 
+				czynszStr = czynszStr + str("2016: "+row[15]+"\n")						
+		try:
+			czynsz17=float(row[16].replace(',','.'))
+		except ValueError:
+			czynsz17 =0
+			if(row[16]): #in case there is a text info 
+				czynszStr = czynszStr + str("2017: "+row[16]+"\n")
+		
+		daty=row[17].split('-')
+		if(daty.__len__()>0):
+			dataPocz=daty[0].replace(" ", "").replace("od","").replace("dnia","")
+		
+		try:
+			data1 = datetime.strptime(dataPocz, '%d.%m.%Y')
+		except ValueError:
+			pass	#zostajemy przy dacie tekstowej
 			
-		#print(str(i)+" "+adres+"\t"+str(czynsz17)+"\t"+row[16])
+		if(daty.__len__()>1):
+			dataKoncowa = daty[1].replace(" ", "").replace("do","").replace("dnia","")
+		
+		try:
+			data2 = datetime.strptime(dataKoncowa, '%d.%m.%Y')
+		except ValueError:
+			pass	#zostajemy przy dacie tekstowej
+		
+		#Podstawa prawna użytkowania (Umowa + aneksy)
+		umowa =str(row[18])
+		
+		#Uwagi dotyczące stanu prawnego
+		stanPrawny	= str(row[19]);
+		#Podstawa prawna nieprzeprowadzania waloryzacji
+		waloryzacja = str(row[20]);
+		
+		#tu można zrobić dodatkową flage do bazy SQL
+		#if((waloryzacja.lower()).find("zwolnienie")>-1):
+		#	print("Zwolnienie "+str(i)+nazwa_podmiotu)
+		
+		#if(waloryzacja.lower().find("jednostka")>-1):
+		#	print("jednostka "+str(i)+nazwa_podmiotu)
+		
+		#if(i>500):
+		#	break
+		'''
+
+		
 '''
 	#	cursor.execute('''INSERT INTO nieruchomosci1(ADRES, nazwa,przeznaczenie,powierzchnia1,info,czynsz)
      #             VALUES(?,?,?,?,?,?)''', (adres,nazwa_podmiotu,przezna,pow2,info,czynsz17))
 	#	db.commit()
 		
 		
-		print(str(i)+" "+adres+"\t added")
+	#	print(str(i)+" "+adres+"\t added")
 
-		i=i+1
+	
