484682468e857f60c0db64ef20e0c0195577cd16
marcinkawka <kawka.marcin@gmail.com>
2018-09-05 20:50:39 +0200

    new web service for obtaining coordinates

--- pobierzCoordinates.py
+++ pobierzCoordinates.py
@@ -8,17 +8,22 @@ import csv
 import sqlite3
 import time
 
-prefix="http://www.punktyadresowe.pl/lokalizacja.php?adres=Warszawa,"
+#to przestało działać
+#prefix="http://www.punktyadresowe.pl/lokalizacja.php?adres=Warszawa,"
+
+
+#to działa w sierpniu 2018
+prefix="http://www.punktyadresowe.pl/ula/?Request=search&searchValue=Warszawa,%2520"
 sufix="&crs=4326&format=json"
 
-adres="ul. Stawki 5/7"
+adres="Stawki 5/7"
 
 url=prefix+urllib.quote(adres)+sufix
 
 
-#response = urllib.urlopen(url)
-#data = json.loads(response.read())
-#pprint(data['wyniki'][0]['wspolrzedne_punktu'])
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+pprint(data['items'][0]['point_on_street_coordinates'][0])
 
 
 
@@ -36,22 +41,26 @@ for m in cursor.execute('SELECT ADRES,ID,LONG,LAT FROM nieruchomosci1 WHERE (lon
 		addr=(addr.split("."))[1]
 		
 	idd=m[1]
-	#print(str(idd)+" "+addr.split(".")[0])
+	print(str(idd)+" "+addr.split(".")[0])
 	url=prefix+(urllib.quote(addr.rstrip())) #rstrip usuwa spacę nakońcu
 	url=url+sufix
 	
-	response = urllib.urlopen(url.encode('utf-8'))
-	data = json.loads(response.read())
+	try:
+		response = urllib.urlopen(url.encode('utf-8'))
+		data = json.loads(response.read())
+
+		if(len(data)>0 and len(data['items'])>0):
+			longg=data['items'][0]['point_coordinates'][0]
+			latt=data['items'][0]['point_coordinates'][1]
+			print("Dodane "+str(longg)+' '+str(latt))
 	
-	if(len(data)>0 and len(data['wyniki'])>0):
-		print("Dodane "+addr.split(".")[0])
-	
-		cursor2.execute('UPDATE nieruchomosci1 set lat = ?, long = ? WHERE ID = ?',(data['wyniki'][0]['wspolrzedne_punktu'][1],data['wyniki'][0]['wspolrzedne_punktu'][0],idd))
-		db.commit()
-	else:
-		print("Nie udało się znaleźć dla "+addr.split(".")[0])
-		print(url)
-			
+			cursor2.execute('UPDATE nieruchomosci1 set lat = ?, long = ? WHERE ID = ?',(latt,longg,idd))
+			db.commit()
+		else:
+			print("Nie udało się znaleźć dla "+addr.split(".")[0])
+			print(url)
+	except ValueError:
+		print("No JSON object")		
 	
 	time.sleep(3)
 
